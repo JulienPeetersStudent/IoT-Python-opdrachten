a62cb4fd62ab414ea59a8ab56a53b30a786622b0
Julien Peeters <julienpeeters.personal@gmail.com>
2022-12-08 02:18:30 +0100

    done!

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,20 @@
 import random
+from os import system as cmd
 
 with open('./words.txt') as wordRead:
     words = wordRead.readlines()
 
+validInputs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 index = random.randint(1, len(words))
 word = words[index]
+wordArray = []
+for x in word:
+    wordArray.extend(x)
+wordArray.pop()
 guesses = []
+wrongGuesses = []
 hangmanVariants = ['''
   +---+
   |   |
@@ -57,15 +66,51 @@ hangmanVariants = ['''
       |
 =========''']
 tries = 0
+invalidInput = False
 while (tries < 6):
+    # clear terminal
+    cmd('cls')
+    # print the ASCII diagram
     print(hangmanVariants[tries])
-    print('_ ' * len(word))
+    # go through each letter, print it if its guessed right, otherwise print _
+    for x in wordArray:
+        if (guesses.__contains__(x)):
+            print(x, end=' ')
+        else:
+            print('_', end=' ')
+    # print all the incorrect letters if the array is longer than 0
+    if (len(wrongGuesses) > 0):
+        print('\n \n [ ', *wrongGuesses, ' ] \n')
+    else:
+        print('\n \n \n')
+    # ask for a letter
+    if (invalidInput):
+        print('Invalid input, punctuation and special characters not allowed!')
     guess = input('Guess a letter: ')
-    if(word.find(guess)):
-        print('word contains the letter')
+    if (validInputs.__contains__(guess) == False):
+        invalidInput = True
+        continue
+    # if guessed right, add letter to guesses array; otherwise add to wrongGuesses and add 1 to tries
+    if (word.__contains__(guess) == True):
+        if (guesses.__contains__(guess) == False):
+            guesses.append(guess)
+        print('correct!')
+        invalidInput = False
+    else:
+        if (wrongGuesses.__contains__(guess) == False):
+            wrongGuesses.append(guess)
+        tries += 1
+        print('wrong...')
+        invalidInput = False
+    # if tries is at 6, cleas terminal and print final ASCII diagram with losing text
+    if (tries == 6):
+        cmd('cls')
+        print(hangmanVariants[tries])
+        print(f'You lost... The word was: {word}.')
         exit()
 
-    if (tries == 6):
+    if (wordArray.__contains__('_') == False):
+        cmd('cls')
         print(hangmanVariants[tries])
-        print('You lost...')
-        exit()
+        print(*wordArray)
+        print('\n ⁂ You won!!! ⁂')
