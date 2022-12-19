b3a008784b0a3026afe9086ccefa41946072c27b
Julien Peeters <s120937@ap.be>
2022-12-19 12:37:57 +0100

    working url

--- graph/meteoGraph.py
+++ graph/meteoGraph.py
@@ -1,13 +1,23 @@
 import turtle as t
-import http.client as web
+import urllib.request as url
+import json
 
 cities = {
    'Antwerp': [51.22, 4.40],
    'Amsterdam': [52.37, 4.89],
    'Berlin': [52.52, 13.41]
 }
+
 for index, x in enumerate(cities):
    print(x)
-city = input('Choose a city: ')
 
-source = web.HTTPConnection('')
+city = input('Choose a city: ')
+cityUrl = ''
+if city in cities:
+   coords = cities[city]
+   cityUrl = f'https://api.open-meteo.com/v1/forecast?latitude={coords[0]}&longitude={coords[1]}&hourly=temperature_2m'
+   print(cities[city])
+   print(cityUrl)
+
+data = url.urlopen(cityUrl)
+print(data)
