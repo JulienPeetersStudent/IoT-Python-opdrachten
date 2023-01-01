fb9db1c1223b06527f57c9d030f9b77d5dba743e
Julien Peeters <s120937@ap.be>
2023-01-01 18:28:20 +0100

    done!

--- graph/meteoGraph.py
+++ graph/meteoGraph.py
@@ -2,6 +2,7 @@ import turtle as t
 import http.client as web
 import json
 
+# list of cities
 cities = {
    'Antwerp': [51.22, 4.40],
    'Amsterdam': [52.37, 4.89],
@@ -10,10 +11,14 @@ cities = {
    'Moscow': [55.75, 37.62]
 }
 
+# print cities for user
 for index, x in enumerate(cities):
    print(x)
 
+# ask for city choice
 city = input('Choose a city: ')
+
+# edit URL for HTTP GET
 cityUrl = ''
 if city in cities:
    coords = cities[city]
@@ -21,17 +26,21 @@ if city in cities:
    print(cities[city])
    print(cityUrl)
 
+# get raw JSON from API
 conn = web.HTTPSConnection("api.open-meteo.com")
 payload = ''
 headers = {}
 conn.request("GET", cityUrl, payload, headers)
 res = conn.getresponse()
-# data = res.read()
+
+# turn raw JSON into usable JSON
 apiJson = json.loads(res.read())
-print(apiJson['hourly']['temperature_2m'])
+# print(apiJson['hourly']['temperature_2m'])
+# setup for graph
 X = 0
 XScale = 2
 YScale = 5
+# drawing the graph by iteration
 for Y in apiJson['hourly']['temperature_2m']:
    t.goto(X * XScale, Y * YScale)
    X += 1
