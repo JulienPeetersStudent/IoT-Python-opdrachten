809c13eaf9abcbf79fe84e2dd999b506c65d3818
Julien Peeters <s120937@ap.be>
2023-01-05 04:09:29 +0100

    graph now handles negative values and maps the correctly, and all text is now white

--- graph/meteoGraph.py
+++ graph/meteoGraph.py
@@ -35,68 +35,84 @@ res = conn.getresponse()
 
 # turn raw JSON into usable JSON
 data = json.loads(res.read())
-# print(data['hourly']['temperature_2m'])
+# print(data)
 
-# setup for graph
+# setup and scaling/centering for graph
 Xmax = len(data['hourly']['temperature_2m'])
 Ymin = min(data['hourly']['temperature_2m'])
 Ymax = max(data['hourly']['temperature_2m'])
 print(Xmax, Ymin, Ymax)
-X = 0
 XScale = 6
 YScale = 40
 graphExt = 25
-graphPadding = 75
+graphPadding = 100
 t.tracer(0, 0)
 t.ht()
 t.bgcolor('#0a0a0a')
 t.Screen().setup(width=(Xmax * XScale) + graphPadding,
-                 height=(Ymax * YScale) + graphExt + graphPadding)
+                 height=((Ymax - Ymin//1) * YScale) + graphExt + graphPadding)
 t.setworldcoordinates(
     -graphPadding / 2,
     -graphPadding / 2,
     (Xmax * XScale) + (graphPadding / 2),
-    (Ymax * YScale) + (graphPadding / 2) + graphExt
-)
-print(t.screensize())
-print(
-    -graphPadding / 2,
-    -graphPadding / 2,
-    (Xmax * XScale) + (graphPadding / 2),
-    (Ymax * YScale) + (graphPadding / 2) + graphExt
+    ((Ymax - Ymin//1) * YScale) + (graphPadding / 2) + graphExt
 )
 
 # draw background grid
 t.pencolor('#888888')
 t.goto(Xmax * XScale, 0)
 t.goto(0, 0)
-t.goto(0, Ymax * YScale + graphExt)
+t.goto(0, (Ymax - Ymin//1) * YScale + graphExt)
 t.up()
-t.pencolor('#444444')
 
-XStripeCount = 0
-XStripeSpacing = 1 * XScale
+# draw Y axis legend
 YStripeCount = 0
 YStripeSpacing = 1 * YScale
+t.pencolor('#444444')
 
-while YStripeCount < ((Ymax * YScale + graphExt) / (YStripeSpacing) - 1):
+while YStripeCount < (((Ymax - Ymin//1) * YScale + graphExt) / (YStripeSpacing) - 1):
     YStripeCount += 1
     t.goto(-10, YStripeCount * YStripeSpacing)
     t.down()
-    t.write(str(YStripeCount) + '°C', align='right')
+    t.pencolor('#ffffff')
+    t.write(str(YStripeCount + (Ymin//1)) + '°C', align='right')
+    t.pencolor('#444444')
     t.goto(Xmax * XScale, YStripeCount * YStripeSpacing)
     t.up()
 
+# draw X axis legend
+XStripeCount = 0
+XStripeSpacing = 1 * XScale
+t.pencolor('#333333')
+
+while XStripeCount < ((Xmax * XScale) / XStripeSpacing - 10):
+    XStripeCount += 10
+    t.goto(XStripeCount * XStripeSpacing, (Ymax - Ymin//1) * YScale + graphExt)
+    t.down()
+    t.goto(XStripeCount * XStripeSpacing, -10)
+    t.up()
+    t.goto(XStripeCount * XStripeSpacing, -25)
+    print(XStripeCount)
+    print((Xmax * XScale) / XStripeSpacing)
+    time = data['hourly']['time'][XStripeCount]
+    t.down()
+    t.pencolor('#ffffff')
+    t.write(time[-5:], align='center')
+    t.pencolor('#333333')
+    t.up()
+
+
 # add city title
 t.pencolor('#ffffff')
-t.goto(0, Ymax * YScale + graphExt)
+t.goto(0, (Ymax - Ymin//1) * YScale + graphExt)
 t.write(city, align='left', font=('Arial', 16, 'bold'))
 
 # drawing the graph by iteration
+Xpos = 0
 for Y in data['hourly']['temperature_2m']:
-    t.goto(X * XScale, Y * YScale)
+    t.goto(Xpos * XScale, (Y * YScale) - (Ymin//1 * YScale))
     t.down()
-    X += 1
+    Xpos += 1
 
 t.up()
 t.home()
