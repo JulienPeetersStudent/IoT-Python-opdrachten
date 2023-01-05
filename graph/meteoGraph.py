cc2ff5f12519fd67f2a67830ae0914bf86a516ec
Julien Peeters <s120937@ap.be>
2023-01-05 02:46:11 +0100

    Added title and auto centering/scaling of window

--- graph/meteoGraph.py
+++ graph/meteoGraph.py
@@ -34,32 +34,39 @@ conn.request("GET", cityUrl, payload, headers)
 res = conn.getresponse()
 
 # turn raw JSON into usable JSON
-apiJson = json.loads(res.read())
-# print(apiJson['hourly']['temperature_2m'])
+data = json.loads(res.read())
+# print(data['hourly']['temperature_2m'])
 
 # setup for graph
-t.screensize(canvwidth=800, canvheight=400, bg='#0a0a0a')
-t.window_height()
+Xmax = len(data['hourly']['temperature_2m'])
+Ymin = min(data['hourly']['temperature_2m'])
+Ymax = max(data['hourly']['temperature_2m'])
+print(Xmax, Ymin, Ymax)
 X = 0
-XScale = 2
+XScale = 4
 YScale = 25
 graphExt = 25
-t.speed('fastest')
+graphPadding = 75
+t.tracer(0, 0)
+t.ht()
+t.bgcolor('#0a0a0a')
+t.Screen().setup(width=(Xmax * XScale) + graphPadding,
+                 height=(Ymax * YScale) + graphExt + graphPadding)
+t.setworldcoordinates(
+    -graphPadding / 2,
+    -graphPadding / 4 + (-graphPadding / 8),
+    (Xmax * XScale) + (graphPadding / 4),
+    (Ymax * YScale) + (graphPadding / 2) + (-graphPadding / 8) + (graphExt * 2)
+)
 
 # draw background grid
-Xmax = len(apiJson['hourly']['temperature_2m'])
-Ymin = min(apiJson['hourly']['temperature_2m'])
-Ymax = max(apiJson['hourly']['temperature_2m'])
-print(Xmax, Ymin, Ymax)
 t.pencolor('#888888')
+t.goto(Xmax * XScale, 0)
+t.goto(0, 0)
 t.goto(0, Ymax * YScale + graphExt)
 t.up()
-t.goto(0, 0)
-t.down()
-t.goto(Xmax * XScale + graphExt, 0)
-t.up()
-t.goto(0, 0)
 t.pencolor('#444444')
+
 XStripeCount = 0
 XStripeSpacing = 1 * XScale
 YStripeCount = 0
@@ -70,14 +77,17 @@ while YStripeCount < ((Ymax * YScale + graphExt) / (YStripeSpacing) - 1):
     t.goto(-10, YStripeCount * YStripeSpacing)
     t.down()
     t.write(str(YStripeCount) + '°C', align='right')
-    t.goto(Xmax * XScale + graphExt, YStripeCount * YStripeSpacing)
+    t.goto(Xmax * XScale, YStripeCount * YStripeSpacing)
     t.up()
-    print(YStripeCount)
-    print((Ymax * YScale + graphExt) / YStripeSpacing)
+
+# add city title
+t.pencolor('#ffffff')
+t.goto(0, Ymax * YScale + graphExt)
+t.write(city, align='left', font=('Arial', 16, 'bold'))
 
 # drawing the graph by iteration
 t.color('#ffffff', '#1c1c1c')
-for Y in apiJson['hourly']['temperature_2m']:
+for Y in data['hourly']['temperature_2m']:
     t.goto(X * XScale, Y * YScale)
     t.down()
     X += 1
